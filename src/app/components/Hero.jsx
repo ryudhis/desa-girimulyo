90c799808536804fe359f293308d280b4a35460b
Bendry Lakburlawal <bendrylakburlawal01@gmail.com>
2024-01-04 23:09:21 +0700

    Update Hero

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -1,14 +1,16 @@
+'use client'
 import React from "react";
-import {Swiper, SwiperSlide} from 'swiper/react';
-import "swiper/css";
+import { Swiper, SwiperSlide} from 'swiper/react';
+import 'swiper/css';
 import "swiper/css/pagination";
-import { Pagination, Autoplay } from "swiper/modules";
+import { Autoplay, Pagination } from 'swiper/modules'
 
 function Hero() {
   const pict = [{ foto: 'images/image1.png' }, { foto: 'images/image2.png' }];
+  //const [swiper, setSwiper] = useState(null)
   return (
-    <div className="flex mt-32" id="hero">
-      <div className="bg-[#1E1E1E] rounded-xl absolute mt-32 right-[7px]">
+    <div className="flex mt-[106px]" id="hero">
+      <button className="bg-[#1E1E1E] rounded-xl absolute mt-[90px] right-[7px] z-10">
         <svg className="justify-end"
           width="18"
           height="18"
@@ -25,23 +27,17 @@ function Hero() {
             />
           </g>
         </svg>
-      </div>
-      <div className="bg-[#1E1E1E] rounded-xl absolute mt-32 left-[7px]">
+      </button>
+      <button className="bg-[#1E1E1E] rounded-xl absolute mt-[90px] left-[7px] z-10">
         <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
           <g id="Expand_left">
           <path id="Vector 9" d="M11.25 4.5L6.75 9L11.25 13.5" stroke="white" stroke-width="2"/>
           </g>
         </svg>
-      </div>
+      </button>
       <div>
         <Swiper
-          slidesPerview={1.2}
-          spaceBetween={20}
-          breakpoints={{
-            768: {
-              slidesPerView: 2,
-            },
-          }}
+          onSwiper={setSwiper}
           loop={true}
           autoplay={{
             delay: 3000,
@@ -49,12 +45,14 @@ function Hero() {
           pagination={{
             clickable: true,
           }}
-          modules={[Pagination, Autoplay]}
+          modules={[Autoplay, Pagination]}
+          //onSwiper={swiper => console.log(swiper)}
+          className='w-[375px]'
         >
           {pict.map((pict_car, i) => (
             <SwiperSlide key={i}>
-              <div key={i}>
-                <img src={pict_car.foto} alt="" />
+              <div className="w-[375px]">
+                <img className="w-full" src={pict_car.foto} alt="" />
               </div>
             </SwiperSlide>
           ))}
