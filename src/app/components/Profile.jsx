83e558a17b1616bf933932a7e6e5106be5114eaa
Juan Verrel Tanuwijaya <126883141+Juan-121140072@users.noreply.github.com>
2024-01-04 14:06:05 +0700

    install daisy

--- src/app/components/Profile.jsx
+++ src/app/components/Profile.jsx
@@ -2,7 +2,12 @@ import React from "react";
 
 const Profile = () => {
   return <div>
-    Profile
+    <div className="text-[#107C41] text-[15px] font-semibold border-b-[1px] border-[#107C41] w-[340px]">
+      <div className="pl-2">Profil Dusun</div>
+    </div>
+    <div className="">
+      
+    </div>
     </div>;
 };
 
