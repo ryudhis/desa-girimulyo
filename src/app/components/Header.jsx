b238d5bf1056b64545e2ddee8a6346618e41ed85
Arya Yudhistira <145851395+ryudhis@users.noreply.github.com>
2024-01-04 13:28:35 +0700

    update template

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { PhoneIcon } from "@heroicons/react/24/solid";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      <div className="flex flex-row justify-between items-end w-screen h-12 bg-[#107C41]">
+        <div>
+          <PhoneIcon />
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
