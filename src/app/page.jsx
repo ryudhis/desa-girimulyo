bd1a5c91e7f0a1c3f7e20b2910fde074a2fd2b99
Juan Verrel Tanuwijaya <126883141+Juan-121140072@users.noreply.github.com>
2024-01-04 13:46:02 +0700

    carousel

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,11 +4,13 @@ import Profile from "./components/Profile";
 import Gallery from "./components/Gallery";
 import Maps from "./components/Maps";
 import Footer from "./components/Footer";
+import Carousel from "./components/Carousel";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <Header />
+      <Carousel />
       <Profile />
       <Gallery />
       <Maps />
