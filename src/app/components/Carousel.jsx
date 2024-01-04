7e38982817b2a4dc63bf8d38bdc832b1b0efb29d
Bendry Lakburlawal <bendrylakburlawal01@gmail.com>
2024-01-04 15:24:39 +0700

    carousel

--- src/app/components/Carousel.jsx
+++ src/app/components/Carousel.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Carousel = () => {
-  return <div>Carousel</div>;
+  return(
+    <div>
+      <div>
+        <div>
+          <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g id="Expand_right">
+            <path id="Vector 9" d="M6.75 4.5L11.25 9L6.75 13.5" stroke="white" stroke-width="2"/>
+            </g>
+          </svg>
+        </div>
+        
+
+      </div>
+    </div>
+  );
 };
 
 export default Carousel;
