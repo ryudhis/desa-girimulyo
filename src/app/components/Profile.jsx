97f31b28924b205c167213492688d1fb40e9d5c3
Juan Verrel Tanuwijaya <126883141+Juan-121140072@users.noreply.github.com>
2024-01-04 15:12:41 +0700

    update profile

--- src/app/components/Profile.jsx
+++ src/app/components/Profile.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 const Profile = () => {
   return (
+<<<<<<< Updated upstream
     <div className="flex flex-col items-center">
       <div className="text-[#107C41] text-[15px] font-bold border-b-[1px] border-[#107C41] w-[340px]">
         <div className="pl-1">Profil Dusun</div>
@@ -9,15 +10,28 @@ const Profile = () => {
 
       <div className="mt-2">
         <div className="text-[11px] font-extrabold text-[#33363F]">
+=======
+    <div className="flex flex-col items-center bg-white">
+      <div className="text-[#107C41] text-[15px] font-semibold border-b-[1px] border-[#107C41] w-[340px]">
+        <div className="pl-2">Profil Dusun</div>
+      </div>
+      <div className="pl-2">
+        <div className="text-[11px] font-extrabold  text-[#33363F]">
+>>>>>>> Stashed changes
           Dusun 10 - Desa Giri Mulyo <br /> Kecamatan Marga Sekampung - Lampung
           Timur
         </div>
 
+<<<<<<< Updated upstream
         <div className="w-[330px] text-[12px] mt-1 leading-3">
+=======
+        <div className="">
+>>>>>>> Stashed changes
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the industry's standard dummy text ever
           since the 1500s, when an unknown printer took a galley of type and
           scrambled it to make a type specimen book. It has survived not only
+<<<<<<< Updated upstream
           five centuries, but also the leap into electronic typesetting.
           <br /> <br />
           Lorem Ipsum is simply dummy text of the printing and typesetting
@@ -29,6 +43,19 @@ const Profile = () => {
           electronic typesetting, remaining essenti. Lorem Ipsum has been the
           industry's standard dummy text ever since the 1500s, when an unknown
           printer took a galley of type and scrambled it.
+=======
+          five centuries, but also the leap into electronic typesetting. 
+          <br /> <br />
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          Lorem Ipsum has been the industry's standard dummy text ever since the
+          1500s, when an unknown printer took a galley of type and scrambled it
+          to make a type specimen book. 
+          <br /> <br />
+          It has survived not only five centuries,
+          but also the leap into electronic typesetting, remaining essenti.
+          Lorem Ipsum has been the industry's standard dummy text ever since the
+          1500s, when an unknown printer took a galley of type and scrambled it.
+>>>>>>> Stashed changes
         </div>
       </div>
     </div>
