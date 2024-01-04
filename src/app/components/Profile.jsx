81db6fd659105747fdabc290a222e554a408bd42
Juan Verrel Tanuwijaya <126883141+Juan-121140072@users.noreply.github.com>
2024-01-04 15:00:53 +0700

    profile dusun

--- src/app/components/Profile.jsx
+++ src/app/components/Profile.jsx
@@ -1,14 +1,38 @@
 import React from "react";
 
 const Profile = () => {
-  return <div>
-    <div className="text-[#107C41] text-[15px] font-semibold border-b-[1px] border-[#107C41] w-[340px]">
-      <div className="pl-2">Profil Dusun</div>
+  return (
+    <div className="flex flex-col items-center">
+      <div className="text-[#107C41] text-[15px] font-bold border-b-[1px] border-[#107C41] w-[340px]">
+        <div className="pl-1">Profil Dusun</div>
+      </div>
+
+      <div className="mt-2">
+        <div className="text-[11px] font-extrabold text-[#33363F]">
+          Dusun 10 - Desa Giri Mulyo <br /> Kecamatan Marga Sekampung - Lampung
+          Timur
+        </div>
+
+        <div className="w-[330px] text-[12px] mt-1">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting.
+          <br /> <br />
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book.
+          <br /> <br />
+          It has survived not only five centuries, but also the leap into
+          electronic typesetting, remaining essenti. Lorem Ipsum has been the
+          industry's standard dummy text ever since the 1500s, when an unknown
+          printer took a galley of type and scrambled it.
+        </div>
+      </div>
     </div>
-    <div className="">
-      
-    </div>
-    </div>;
+  );
 };
 
 export default Profile;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,7 +8,7 @@ import Carousel from "./components/Carousel";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
+    <main className="flex min-h-screen flex-col items-center justify-between bg-white">
       <Header />
       <Carousel />
       <Profile />
