5923e8dbd8881f8e3215a59d6c93986d94db42cf
121140189-MuhammadFabil <muhammadfabil40@gmail.com>
2024-01-04 16:45:15 +0700

    test

--- src/app/components/Gallery.jsx
+++ src/app/components/Gallery.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 
 const Gallery = () => {
+  
   return (
     <div>
-      <h1>Galeri Dusun 101</h1>
-      {/* judul */}
-      <div>
-        {/* Foto */}
-        {/* Info */}
+      <h1 className="text-[#107C41] mx-3 border-b-2 border-[#107C41]">Galeri Dusun 10</h1>
+      <div className="flex justify-center">
+        <div className="mt-[9px] w-[335px] h-[119px]" >
+          <img className="object-none object-bottom " src='images/img_galeri1.png' />
+        </div>
       </div>
+      
     </div>
     
   );
