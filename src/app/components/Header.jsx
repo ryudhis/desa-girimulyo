584d48802e882c4868d7090e32fef615f61d6216
Arya Yudhistira <145851395+ryudhis@users.noreply.github.com>
2024-01-04 13:59:41 +0700

    Update Header.jsx

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -8,15 +8,26 @@ import {
 const Header = () => {
   return (
     <div>
-      <div className="flex flex-row justify-between items-center w-screen h-12 bg-[#107C41]">
-        <div className="flex flex-row justify-center items-center">
-          <PhoneIcon className="h-4 w-4 text-white" />
-          <p className="text-white text-sm">0831-88889999</p>
-          <EnvelopeIcon className="h-4 w-4 text-white" />
+      <div className="flex flex-row px-4 justify-between items-center w-screen h-12 bg-[#107C41]">
+        <div className="flex flex-row gap-1 justify-center items-center">
+          <PhoneIcon className="h-3 w-3 text-white" />
+          <p className="text-white text-[8px] font-extralight">0831-88889999</p>
+          <p className="text-white text-[8px] font-extralight">|</p>
+          <EnvelopeIcon className="h-3 w-3 text-white" />
+          <p className="text-white text-[8px] font-extralight">
+            emaildesa@gmail.co.id
+          </p>
         </div>
-        <div>
-          <GlobeAsiaAustraliaIcon />
+        <div className="flex flex-row gap-1 justify-center items-center">
+          <GlobeAsiaAustraliaIcon className="h-3 w-3 text-white" />
+          <p className="text-white text-[8px] font-extralight">
+            Kabupaten Lampung Timur
+          </p>
         </div>
+      </div>
+      <div className="w-screen bg-white h-14 flex flex-row justify-between items-center">
+        <div></div>
+        <div></div>
       </div>
     </div>
   );
